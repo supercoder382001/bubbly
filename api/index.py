3fc9e331eb3fa332041016706a13525f8b5277be
supercoder382001 <63428439+supercoder382001@users.noreply.github.com>
2024-08-25 00:50:28 +0530

    Add files via upload

--- api/index.py
+++ api/index.py
@@ -1,20 +1,11 @@
 # import requests
 # import json
 import json
-import importlib
-import sys
+
 from flask import Flask,request,jsonify
 # from initiate import initiate_
 # from processorder import process_
 # from validatevpa import validate_
-from os.path import dirname, abspath, join
-current_dir = dirname(abspath(__file__))
-initiate_path = join(current_dir, 'processorder.py.py')
-spec = importlib.util.spec_from_file_location("initiate", initiate_path)
-initiate = importlib.util.module_from_spec(spec)
-sys.modules["initiate"] = initiate
-spec.loader.exec_module(initiate)
-result=initiate.process_()
 
 app = Flask(__name__)
 
@@ -33,7 +24,6 @@ def create():
 
 @app.route('/Process',methods=['POST'])
 def process():
-    dir=dirname(abspath())
     data = {
         "message": True,
         "url": "ABC"
@@ -46,4 +36,4 @@ def validate():
         "message": True,
         "url": "ABC"
     }
-    return result
+    return json.dumps(data)
