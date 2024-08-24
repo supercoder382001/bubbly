252a071b1ad7d19d5654759d0f9e96c92fbd3579
supercoder382001 <63428439+supercoder382001@users.noreply.github.com>
2024-08-25 00:21:23 +0530

    Add files via upload

--- api/index.py
+++ api/index.py
@@ -2,22 +2,29 @@
 # import json
 from flask import Flask,request,jsonify
 # from initiate import initiate_
-from processorder import process_
-from validatevpa import validate_
+# from processorder import process_
+# from validatevpa import validate_
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
 
-# @app.route('/create',methods=['POST'])
-# def create():
-#     return initiate_()
+@app.route('/create',methods=['POST'])
+def create():
+    return initiate_()
 
 @app.route('/Process',methods=['POST'])
 def process():
-    return process_()
-
-# @app.route('/Validate',methods=['GET'])
-# def validate():
-#     return validate_()
+    data = {
+        "message": True,
+        "url": "ABC"
+    }
+    return data.json()
+@app.route('/Validate',methods=['GET'])
+def validate():
+    data = {
+        "message": True,
+        "url": "ABC"
+    }
+    return data.json()
